753ed87ecda45882a2584abd2738a640289d77d8
Richard Duque <richy9.13@gmail.com>
2020-09-09 22:56:00 -0500

    primer commit

---
+++ app_old.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+http.createServer((req, resp) => {
+
+        resp.writeHead(200, { 'Content-Type': 'application/json' });
+
+        let salida = {
+            nombre: 'Richard',
+            edad: 32,
+            url: req.url
+        }
+
+        resp.write(JSON.stringify(salida))
+        resp.end();
+    })
+    .listen(8080);
+
+console.log('Escuchando el puerto 8080');
